425c1fc8e39ca7cfa7643fb137f7299d47afb382
an-gj-dy <ante.dybedahl@gmail.com>
2023-02-10 20:00:44 +0100

    Finalized typewriter animation

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 function typeWriter(selector, interval) {
     const stringContainer = document.querySelector(selector);
     const stringToAnimate = Array.from(stringContainer.innerHTML);
-    let finished = false;
     stringContainer.innerHTML = "";
+    finishedTyping = false;
     let letterCount = 0;
     let intervalID = setInterval(() => {
         if (letterCount < stringToAnimate.length) {
             stringContainer.innerHTML += stringToAnimate[letterCount];
             letterCount++;
         } else if ((letterCount = stringToAnimate.length)) {
+            console.log(intervalID);
             clearInterval(intervalID);
-            finished = true;
         }
     }, interval);
 }
 
-window.addEventListener("DOMContentLoaded", typeWriter("h1", 100));
+const fullNameTypeWriter = typeWriter("h1", 100);
